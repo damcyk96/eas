716b9017b3355e02c3b9e96c8cd2e4ae083dba9a
damcyk96 <damian.obajtek@gmail.com>
2023-04-14 10:45:15 +0200

    basic app

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          fontSize: 24,
+          marginBottom: 16,
+        }}
+      >
+        Kajetan 5th birthday
+      </Text>
+      <Image
+        source={{
+          uri: "https://betteroffinside-production.s3.us-east-2.amazonaws.com/uploads/b2d4d1be5ed43ecaccb42b59664dcf94.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAXAFQ566ASFWYOAOL%2F20230414%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20230414T082548Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=a2680f96f1c2fc6af298947c7c20565d72b6c6291290d91c927f776d52827974",
+        }}
+        style={{ width: "90%", height: 400 }}
+        resizeMode="contain"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
